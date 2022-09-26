7a5a32e53f45c5f8f271b1ed016c46007de91546
Brett g Porter <brett@bgporter.net>
2022-09-26 12:53:25 -0400

    Add testSuite.h and python utility.

---
+++ makeTest.py
@@ -0,0 +1,156 @@
+#! /usr/bin/env python3
+
+'''
+   makeTest.py -- tiny utility to create a skeleton JUCE UnitTest class.
+   Copyright (c) 2018 Brett g Porter
+
+   Given the command line param 'ClassName', will create a file
+   `test_ClassName.cpp` either in :
+      - a directory named `test` underneath the current directory.
+      - in the current directory, if we're already in a directory named `test`.
+   It will also attempt to open a file `className.cpp` and append the necessary
+   code to include the code in the newly created test file. 
+   
+   **NOTE** that the macro `RUN_UNIT_TESTS` must be defined and equal to 1 
+   in the source file that wants to be tested.    
+   
+'''
+
+import os
+
+kTemplate = """
+
+#include <juce_core/juce_core.h>
+
+class Test_{0} : public TestSuite
+{{
+public:
+    Test_{0}() 
+    : TestSuite("{0}", "!!! category !!!")
+    {{
+
+    }}
+
+    void runTest() override
+    {{
+        beginTest("!!! WRITE SOME TESTS FOR THE {0} Class !!!");
+
+        /*
+          To create a test, call `test("testName", testLambda);`
+          To (temporarily) skip a test, call `skipTest("testName", testLambda);`
+          To define setup for a block of tests, call `setup(setupLambda);`
+          To define cleanup for a block of tests, call `tearDown(tearDownLambda);`
+
+          Setup and TearDown lambdas will be called before/after each test that 
+          is executed, and remain in effect until explicitly replaced. 
+
+          All the functionality of the JUCE `UnitTest` class is available from
+          within these tests. 
+        */
+    }}
+private:
+    // !!! test class member vars here...
+}};
+
+static Test_{0} test{0};
+"""
+
+kCppTemplate = """
+#if RUN_UNIT_TESTS
+#include "test/test_{0}.cpp"
+#endif
+"""
+
+
+def CreateTestFile(className):
+    pth = os.getcwd()
+    head, currentDir = os.path.split(pth)
+    if currentDir != 'test':
+        # We're not already in the test directory.
+        try:
+            pth = os.path.join(pth, "test")
+            print ("Creating dir {}".format(pth))
+            os.mkdir(pth)
+        except OSError:
+            # the dir already exists, ignore.
+            pass
+    fileName = "test_{}.cpp".format(className)
+    fullPath = os.path.join(pth, fileName)
+    if (not os.path.exists(fullPath)):
+       print ("Creating file {}".format(fullPath))
+       with open(fullPath, "wt") as f:
+           f.write(kTemplate.format(className))
+       return True
+    else:
+       print ("test file already exists -- not creating it.")
+       return False
+
+def AddTestInclude(className):
+    ''' attempt to open the cpp source file being tested and append the 
+        chunk of code that conditionally includes the unit tests.
+    '''
+
+    def FindSource(path, className):
+        '''
+            Look for a file named either "ClassName.cpp" or "className.cpp" in the 
+            directory specified by 'path' and return its full path. 
+
+            Will raise FileNotFoundError if appropriate. 
+        '''
+        fileName = className[0].upper() + className[1:] + ".cpp"
+        fullPath = os.path.join(pth, fileName)
+        if os.path.exists(fullPath):
+            return fullPath
+
+        # try again with leading lowercase:
+        fileName = className[0].lower() + className[1:] + ".cpp"
+        fullPath = os.path.join(pth, fileName)
+        if os.path.exists(fullPath):
+            return fullPath
+
+        raise FileNotFoundError
+
+    pth = os.getcwd()
+    head, currentDir = os.path.split(pth)
+    if currentDir == 'test':
+        # we are in the test directory already, need to look above where we are.
+        pth = head
+
+    try:
+        fullPath = FindSource(pth, className)
+        print ("Adding include of the unit tests.")
+        with open(fullPath, "at") as f:
+            f.write(kCppTemplate.format(className))
+        return True
+    except FileNotFoundError:
+        print ("ERROR: {} not found".format(fullPath))
+        return False
+
+def Usage():
+    print (""" makeTest.py <ClassName>
+
+    1. Creates a unit test file for the specified class in a directory 
+       named "test" underneath the directory containing that class file
+       (unless the file already exists).
+    2. Inserts a block of code at the end of the specified class to include
+       the contents of the unit test file inside the cpp file. 
+
+    ASSUMES that the class "FooClass" is contained in a file that's named
+    `fooClass.cpp`
+    
+    """)
+
+
+if __name__ == "__main__":
+    import sys
+    try:
+        className = sys.argv[1]
+    except IndexError:
+        className = "--help"
+
+    if "--help" == className:
+        Usage()
+        sys.exit(0)
+        
+    if CreateTestFile(className):
+       AddTestInclude(className)
---
+++ testSuite.h
@@ -0,0 +1,198 @@
+//
+// Copyright (c) 2021 Brett g Porter. All Rights Reserved.
+//
+
+#pragma once
+
+#include <juce_core/juce_core.h>
+
+/**
+ * @class TestSuite
+ * @brief An extension to the JUCE UnitTest system.
+ */
+
+class TestSuite : public juce::UnitTest
+{
+  public:
+    TestSuite(const juce::String &name, const juce::String &category = juce::String()) : UnitTest(name, category)
+    {
+    }
+
+    /**
+     * Pass in a function to perform common setup required by multiple
+     * tests.
+     *
+     * Called automatically before each call of the `test()` method, remains
+     * in effect until a new setup function is passed to the test class.
+     *
+     * If your test class needs resources that are allocated once for
+     * all of your subtests, you can handle that by overriding the
+     * `UnitTest::initialise()` method.
+     *
+     * Default does nothing.
+     */
+    void setup(std::function<void(void)> setupFn)
+    {
+        onSetup = setupFn;
+    }
+
+    /**
+     * Pass in a function to perform any common cleanup needed by your subtests.
+     *
+     * If your test class allocated resources in the `initialise()` method that
+     * stayed in scope for all of your subtests, you should handle that cleanup
+     * by overriding the `UnitTest::shutdown()` method.
+     *
+     * If the class you're testing has a method that lets you check a class
+     * invariant, adding a call inside the `TearDown()` method like:
+     * ```
+     *    // call your class invariant checker
+     *    expect (this->IsValid());
+     * ```
+     *
+     * Lets you check that each test not only succeeded on its own terms, but
+     * left the object being tested in a valid state.
+     */
+    void tearDown(std::function<void(void)> tearDownFn)
+    {
+        onTearDown = tearDownFn;
+    }
+
+    /**
+     * The Test method:
+     * * performs common setup.
+     * * executes a single subtest
+     * * performs the common cleanup.
+     *
+     * Your unit tests will be a sequence of calls to `test(...)` inside of your
+     * test suite class' `runTest()` method.
+     *
+     * @param testName Name of the sub-test.
+     * @param testFn   Test function to call.
+     * @sa SkipTest
+     */
+    void test(juce::StringRef testName, std::function<void(void)> testFn)
+    {
+        beginTest(testName);
+        onSetup();
+        testFn();
+        onTearDown();
+    }
+
+    /**
+     * An easy way to disable a test that's implemented by calling an std::function
+     * that doesn't just comment (or conditionally compile) out that test. Your
+     * test log will include a line helping you remember that the test wasn't run.
+     *
+     * Assumption is that you may encounter working tests that momentarily break
+     * so we want to temporarily disable them. Instead of commenting or `#ifdef`-ing
+     * those tests out, change the function call from `test(...)` to
+     * `skipTest(...)`. To re-enable the test, just change that method call back
+     * to `test(...)`.
+     *
+     * @param testName name/description of this test.
+     * @param testFn   test function to (not) call.
+     *
+     * @sa Test
+     */
+    void skipTest(juce::StringRef testName, std::function<void(void)> /*testFn*/)
+    {
+        logMessage("-----------------------------------------------------------------");
+        logMessage(TRANS("WARNING: Skipping ") + getName() + " / " + testName);
+    }
+
+    /// default setup/tearDown do nothing.
+    inline static std::function<void(void)> noOp{[]() {}};
+
+    inline static const juce::String QuitAfterTests{"--quitAfterTests"};
+    inline static const juce::String DisableTests{"--disableTests"};
+    inline static const juce::String EnableTests{"--enableTests"};
+    inline static const juce::String AssertOnFail{"--assertOnFail"};
+    inline static const juce::String ContinueOnFail{"--continueOnFail"};
+    inline static const juce::String LogPasses{"--logPasses"};
+    inline static const juce::String RandomSeed{"--randomTestSeed"};
+
+    /**
+     * @brief Load and run all of the unit tests defined in this project.
+     *
+     * Meant to be called from the top of your app's `initialise()`
+     * method and passed the command line that's provided to your
+     * application.
+     *
+     * May be completely disabled by setting the macro RUN_UNIT_TESTS
+     * to zero.
+     *
+     * DEFAULT behaviors:
+     * in all cases:
+     *     - only failures are logged (unless cmdline flag `--logPasses`)
+     *     - random seed for tests is 0 (unless cmdline flag `--randomTestSeed <<intVal>>`)
+     *     - returns true to continue unless cmdline flag `--quitAfterTests`)
+     *
+     * in debug builds (JUCE_DEBUG is set):
+     *     - all tests are run (unless cmdline flag `--disableTests`)
+     *     - first test fail asserts (unless cmdline flag `--continueOnFail`)
+     *
+     * in release builds (JUCE_DEBUG is not set):
+     *     - no tests run (unless cmdline flag `--enableTests`)
+     *     - always continue after test failure (unless cmdline flag `--assertOnFail`)
+     *
+     * e.g.:
+     *
+     *          void MyApplication::initialise (const String& commandLine)
+     *          {
+     *               if (! TestSuite::runAllTests (commandLine))
+     *                   quit ();
+     *               // ...rest of initialise code...
+     *          }
+     *
+     * @param commandLine   as defined above
+     * @return true         to tell application to continue normally
+     * @return false        to tell application it should exit immediately.
+     */
+
+    static bool runAllTests(const juce::String &commandLine)
+    {
+        bool continueRunning{true};
+#if RUN_UNIT_TESTS
+        bool runTests{false};
+        juce::StringArray commands{juce::StringArray::fromTokens(commandLine, true)};
+        continueRunning = (-1 == commands.indexOf(TestSuite::QuitAfterTests, true));
+        bool logPasses{-1 != commands.indexOf(TestSuite::LogPasses, true)};
+        bool assertOnFail{true};
+        juce::int64 randomSeed{0};
+        if (auto seedIndex{commands.indexOf(TestSuite::RandomSeed, true)}; (seedIndex > -1))
+        {
+            // will return an empty string if the index is out of bounds...
+            auto &seedValue{commands[seedIndex + 1]};
+            randomSeed = seedValue.getLargeIntValue();
+        }
+
+#if JUCE_DEBUG
+        // in debug builds, run tests UNLESS command line turns them off.
+        runTests = (-1 == commands.indexOf(TestSuite::DisableTests, true));
+        // in debug builds, assert on fail unless the continue flag is present
+        assertOnFail = (-1 == commands.indexOf(TestSuite::ContinueOnFail, true));
+#else
+        // in release builds, don't run tests unless explicitly run.
+        runTests = (-1 != commands.indexOf(TestSuite::EnableTests, true));
+        // in release builds, continue on test faiure unless the assert flag is set.
+        assertOnFail = (-1 != commands.indexOf(TestSuite::AssertOnFail, true));
+#endif
+
+        if (runTests)
+        {
+            juce::UnitTestRunner testRunner;
+            testRunner.setAssertOnFailure(assertOnFail);
+            testRunner.setPassesAreLogged(logPasses);
+            testRunner.runAllTests(randomSeed);
+        }
+#endif
+        return continueRunning;
+    }
+
+  private:
+    /// function to be called before each test.
+    std::function<void(void)> onSetup{noOp};
+    /// function to be called after each test.
+    std::function<void(void)> onTearDown{noOp};
+};
